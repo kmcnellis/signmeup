f233cef93fe8743b1f6caee0f0123339a737b49f
Sarah Schattschneider <schats@rpi.edu>
2015-04-14 17:30:55 -0400

    Tabs are working, the navigation bar is now on the top of the of the reoccuring page.

--- client/app/reoccurring/reoccurring.controller.js
+++ client/app/reoccurring/reoccurring.controller.js
@@ -2,28 +2,33 @@
 
 angular.module('signmeupApp')
   .controller('ReoccurringCtrl', function ($scope, $http) {
-    $scope.data = [];
 
-    $http.get('/app/reoccurring').success(function(data) {
-      $scope.data = data;
-    });
-  })
+   //practicing with this array of times to volunteer on Monday
+    var monday = [
+      { time: '2:30pm', job: 'dishes'},
+      { time: '9:00am', job: 'serving'}
+    ];
 
-  .controller('TabController', function($scope){
+    $scope.data = monday;
+
+    // $http.get('/api/reoccurring').success(function(data) {
+    //   $scope.data = data;
+    // });
+
+    //set the originail tab value to 1
     $scope.tab = 1;
 
+    //set the tab with a new value
     $scope.setTab = function(newValue){
       $scope.tab = newValue;
     };
 
-    $scope.isSet = function(tabName){
-      return $scope.tab == tabName;
-
+    //checking if tab is set to a value
+    $scope.isSet = function(tabValue){
+      return $scope.tab == tabValue;
     };
+   
   })
 
 
-var monday = [
-  { time: '2:30pm', job: 'dishes'},
-  { time: '9:00am', job: 'serving'}
-];
+
